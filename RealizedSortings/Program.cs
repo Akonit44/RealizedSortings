cae3b376e451ed8ead3d8ef39540fd78158a9b87
Akonit44 <for_iphone91@mail.ru>
2021-03-30 22:06:02 +0300

    WideSearch begin

--- RealizedSortings/Program.cs
+++ RealizedSortings/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,29 @@ namespace RealizedSortings
     {
         static void Main(string[] args)
         {
+            Hashtable hash = new Hashtable();
+            Queue queue_lst = new Queue();
+
+            hash.Add("you", new string[] { "alice", "bob", "claire" });
+            hash.Add("alice", new string[] { "bob", "claire" });
+
+            Add_to_Queue(hash, ref queue_lst, "you");
+            Add_to_Queue(hash, ref queue_lst, Get_Key(ref queue_lst));
+
+            for (;  queue_lst.Count !=0; )
+                Console.WriteLine($"{queue_lst.Dequeue()}");
+            
+        }
+        public static void Add_to_Queue(Hashtable hash, ref Queue queue, string key)
+        {
+            foreach (string neighbour in (string[])hash[key])
+            {
+                if (!queue.Contains(neighbour)) queue.Enqueue(neighbour);
+            }
+        }
+        public static string Get_Key(ref Queue queue)
+        {
+            return (string)queue.Dequeue();
         }
     }
 }
---
+++ RealizedSortings/SearchMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace RealizedSortings
+{
+    public class SearchMethods
+    {
+        public static string wideSearch()
+        {
+            Queue a = new Queue();
+            return null;
+        }
+    }
+}
