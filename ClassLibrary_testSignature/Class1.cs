c334d9a28a71255d512694adfd04ab43002b73d0
Akonit44 <for_iphone91@mail.ru>
2021-04-21 15:03:56 +0300

    sef

---
+++ ClassLibrary_testSignature/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_testSignature
+{
+    public class Class1
+    {
+        /// <summary>
+        /// Выводит "это работает на англ"
+        /// </summary>
+        public static void Show()
+        {
+            Console.WriteLine("It's work!!");
+        }
+    }
+}
